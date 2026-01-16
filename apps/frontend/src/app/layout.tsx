41dd58f1a46d591c0d2ecdc3680047dd0be9f4b4
runru1030 <runru1030@gmail.com>
2026-01-16 20:08:31 +0900

    chore : v4 설정 방식으로 수정

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./styles/globals.css";
 import ReactQueryProvider from "./ReactQueryProvider";
 
 export const metadata: Metadata = {
