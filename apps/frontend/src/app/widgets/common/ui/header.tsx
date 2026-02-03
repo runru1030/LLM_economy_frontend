f9b6f69ed853542d716baa0e55aa35509d07b1d8
runru1030 <runru1030@gmail.com>
2026-02-03 20:59:56 +0900

    chore : nav, header 수정

--- apps/frontend/src/app/widgets/common/ui/header.tsx
+++ apps/frontend/src/app/widgets/common/ui/header.tsx
@@ -1,23 +1,63 @@
 "use client";
-import dynamic from "next/dynamic";
+import ThreadListDrawer from "@widgets/economy-agent/ui/thread-list-drawer";
+import Link from "next/link";
 import { useSelectedLayoutSegments } from "next/navigation";
+import React, { useMemo } from "react";
+import { MdArrowBackIos } from "react-icons/md";
 
-const ThreadListDrawer = dynamic(() => import("@widgets/economy-agent/ui/thread-list-drawer"), {
-  ssr: false,
-});
+enum SEGMENT {
+  HOME = "home",
+  ECONOMY_AGENT = "economy-agent",
+}
+const HEADER_CONTENT = {
+  [SEGMENT.HOME]: {
+    leftItem: undefined,
+    rightItem: undefined,
+    text: "LLM economy",
+  },
+  [SEGMENT.ECONOMY_AGENT]: {
+    leftItem: () => (
+      <Link href={"/"}>
+        <MdArrowBackIos />
+      </Link>
+    ),
+    rightItem: (threadId: string | null) => <ThreadListDrawer threadId={threadId} />,
+    text: "economy Agent",
+  },
+} as const;
+
+function HeaderContainer({
+  text,
+  leftItem,
+  rightItem,
+}: {
+  text?: string;
+  leftItem?: React.ReactNode;
+  rightItem?: React.ReactNode;
+}) {
+  return (
+    <div className="h-15 flex items-center sticky top-0 left-0 px-4 bg-white border-b border-gray-100  font-extralight gap-2.5 justify-between text-gray-800">
+      <div className="flex items-center gap-2">
+        {leftItem}
+        {text}
+      </div>
+      {rightItem}
+    </div>
+  );
+}
 export default function Header() {
   const segments = useSelectedLayoutSegments();
-  const OptionalLeftComponent = () => {
-    if (segments.includes("economy-agent")) {
-      const threadId = segments.length >= 2 ? segments[1] : null;
-      return <ThreadListDrawer threadId={threadId} />;
-    }
-    return null;
-  };
+  const key = useMemo(() => {
+    if (segments.includes("economy-agent")) return SEGMENT.ECONOMY_AGENT;
+    return SEGMENT.HOME;
+  }, [segments]);
+  const resourceId = segments.length >= 2 ? segments[1] : null;
+
   return (
-    <div className="h-15 flex items-center sticky top-0 left-0 px-4 bg-white border-b border-gray-100  font-extralight gap-2.5">
-      <OptionalLeftComponent />
-      LLM economy
-    </div>
+    <HeaderContainer
+      leftItem={HEADER_CONTENT[key].leftItem?.()}
+      rightItem={HEADER_CONTENT[key].rightItem?.(resourceId)}
+      text={HEADER_CONTENT[key].text}
+    />
   );
 }
